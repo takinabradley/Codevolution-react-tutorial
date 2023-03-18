79a3f0f093dc083404f3ead2584e993e40993607
takinabradley <bradfinney98@gmail.com>
2023-03-18 07:03:56 -0400

    Add notes about useEffect cleanup behavior

--- src/hook-components/IntervalHookCounter.js
+++ src/hook-components/IntervalHookCounter.js
@@ -1,14 +1,26 @@
 import React, { useEffect, useState } from "react"
 export default function IntervalHookCounter() {
   const [count, setCount] = useState(0)
-  const tick = () => setCount((prevCount) => prevCount + 1)
+  const tick = () => {
+    console.log("tick")
+    setCount((prevCount) => prevCount + 1)
+  }
 
   useEffect(() => {
+    // the useEffect cleanup function happens every re-render
+    // and so this doesn't actually cause intervals to stack.
+
+    // if we check prevState, like we do now, we can actually safely
+    // remove count as a dependency so this effect is only applied once
+    // and is cleaned up when the component is unmounted.
     console.log("hi")
     const interval = setInterval(tick, 1000)
 
-    return () => clearInterval(interval)
-  }, [])
+    return () => {
+      console.log("cleaning up")
+      clearInterval(interval)
+    }
+  }, [count])
 
   return <div>{count}</div>
 }
