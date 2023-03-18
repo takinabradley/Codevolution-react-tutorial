f8c3de5b3c9a9fbd1db793626d7aea2ba1f5d59b
takinabradley <bradfinney98@gmail.com>
2023-03-18 09:09:37 -0400

    Create merge reducer function for similar behavior to class based react

--- src/hook-components/CounterOne.js
+++ src/hook-components/CounterOne.js
@@ -14,15 +14,30 @@ const reducer = (currentState, action) => {
   }
 }
 
+const mergeObject = (currentState, propObj) => {
+  const newState = { ...currentState }
+  for (const key in propObj) {
+    if (propObj[key] === currentState[key]) continue
+    newState[key] = propObj[key]
+  }
+  return newState
+}
+
 export default function CounterOne() {
-  const [count, dispatchCount] = useReducer(reducer, 0)
+  const [state, setState] = useReducer(mergeObject, { a: 1, b: 2, c: 3 })
+
+  const stateData = Object.entries(state).map((entry, index) => (
+    <div key={index}>
+      {entry[0]} {entry[1]}
+    </div>
+  ))
 
   return (
     <div>
-      {count}
-      <button onClick={() => dispatchCount("increment")}>Increment</button>
-      <button onClick={() => dispatchCount("decrement")}>Decrement</button>
-      <button onClick={() => dispatchCount("reset")}>Reset</button>
+      {stateData}
+      <button onClick={() => setState({ a: state.a + 1 })}>Increment</button>
+      <button onClick={() => setState({ b: state.b - 1 })}>Decrement</button>
+      <button onClick={() => setState({ a: 1, b: 2, c: 3 })}>Reset</button>
     </div>
   )
 }
